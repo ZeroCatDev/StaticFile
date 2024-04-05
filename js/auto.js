3efa38291b4cff80869ccd59eac9dcf6c5be2f47
SunWuyuan <1847261658@qq.com>
2024-04-05 21:50:27 +0800

    1

--- js/auto.js
+++ js/auto.js
@@ -3,5 +3,8 @@ function automsg(msg) {
     if (typeof msg === 'string') {
       msg = { message: msg };
     }
-    mdui.snackbar(msg);
+    mdui.snackbar({
+      message: msg.message,
+      closeable: true,
+    });
   }
